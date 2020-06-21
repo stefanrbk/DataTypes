15ad01b416d9ab5a5ec601a367e4b1d7be2a96c0
Stefan Kewatt <stefan.kewatt@gmail.com>
2020-06-21 15:47:47 -0500

    Updated tests layout again

---
+++ DataTypes/NumericTypesTests/Half/HalfMethods.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+using static System.Half;
+
+namespace System.Tests
+{
+    [TestFixture]
+    public static class HalfMethods
+    {
+        [Test]
+        public static void get_bytes()
+        {
+            byte[] expected = { 0x22, 0x33 };
+            var value = (Half)0.2229004f;
+            var actual = GetBytes(value);
+
+            TestContext.WriteLine($"Half.GetBytes({value});\n" +
+                                  $"Expected: '{expected}'\n" +
+                                  $"Actual: '{actual}'");
+
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public static void get_hash_code()
+        {
+            var value = TestContext.CurrentContext.Random.NextUShort();
+            var target = ToHalf(BitConverter.GetBytes(value), 0);
+            var expected = HashCode.Combine(value, 5);
+            var actual = target.GetHashCode();
+
+            TestContext.WriteLine($"({target}).GetHashCode();\n" +
+                                  $"Expected: '{expected}'\n" +
+                                  $"Actual: '{actual}'");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ DataTypes/NumericTypesTests/Half/HalfParityWithSingle.cs
@@ -0,0 +1,380 @@
+﻿using NUnit.Framework;
+using System.Globalization;
+using System.Linq;
+
+using static System.Half;
+
+namespace System.Tests
+{
+    [TestFixture(TestOf = typeof(Half))]
+    public static class HalfParityWithSingle
+    {
+        public static readonly (Half, float)[] ValueTarget =
+        {
+            (72, 72),
+            (PositiveInfinity, PositiveInfinity),
+            (NegativeInfinity, NegativeInfinity),
+            (NaN, NaN),
+            (-42, -42),
+            (Epsilon, Single.Epsilon)
+        };
+        public static readonly Half[] ToStringTargets =
+        {
+            MathH.PI,
+            10000,
+            (Half)(-5.5)
+        };
+        public static readonly string[] FormatTargets =
+        {
+            "C",
+            "e",
+            "n",
+            "##;(##);[  ##  ]"
+        };
+        public static readonly IFormatProvider[] ProviderTargets =
+        {
+            new CultureInfo("en-US"),
+            new CultureInfo("fr-FR"),
+            new CultureInfo("en-GB"),
+            new CultureInfo("ja-JP")
+        };
+        public static readonly string[] ParseTargets = ToStringTargets
+                                                           .Select(a => a.ToString())
+                                                           .ToArray();
+        public static readonly (string, IFormatProvider)[] ParseWithProviderTargets =
+            ToStringTargets
+                .SelectMany(a => ProviderTargets
+                                     .Select(b => a.ToString(b)))
+                .Zip(ToStringTargets
+                         .SelectMany(_ => ProviderTargets))
+                .ToArray();
+        public static readonly IFormatProvider[] ProviderExpandedTargets = ToStringTargets
+                                                     .SelectMany(_ => ProviderTargets
+                                                                          .AsEnumerable())
+                                                     .ToArray();
+        public static readonly (Half, Half)[] CompareToHalfTargets =
+        {
+            (1, 2),
+            (666,555),
+            (7,7)
+        };
+        public static readonly (Half, object, object)[] CompareToObjectTargets =
+        {
+            (1, (Half)2f, 2f),
+            (666, (Half)555f, 555f),
+            (7, (Half)7f, 7f)
+        };
+
+        [Test]
+        public static void compare_to(
+                               [ValueSource("CompareToHalfTargets")] (Half left, Half right) target)
+        {
+            var actual = target.left.CompareTo(target.right);
+            var expected = ((float)target.left).CompareTo(target.right);
+            (var testLeft, var testRight) = (target.left.ToString(), target.right.ToString());
+
+            TestContext.WriteLine($"(Half){testLeft}.CompareTo((Half){testRight})\n" +
+                                  $"(Single){testLeft}.CompareTo((Single){testRight})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void compare_to(
+                               [ValueSource("CompareToObjectTargets")] (Half left, object halfRight, object singleRight) target)
+        {
+            var actual = target.left.CompareTo(target.halfRight);
+            var expected = ((float)target.left).CompareTo(target.singleRight);
+            (var testLeft, var testRight) = (target.left.ToString(), target.halfRight.ToString());
+
+            TestContext.WriteLine($"(Half){testLeft}.CompareTo((Object){testRight})\n" +
+                                  $"(Single){testLeft}.CompareTo((Object){testRight})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void compare_to()
+        {
+            var expected = Assert.Catch(typeof(ArgumentException),
+                                        () => 1f.CompareTo(new object()));
+            var actual = Assert.Catch(typeof(ArgumentException),
+                                      () => ((Half)1).CompareTo(new object()));
+            TestContext.WriteLine($"(Half).CompareTo(object)\n" +
+                                  $"(Single).CompareTo(object)\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+        }
+
+        [Test]
+        public static void equals(
+                               [Range(0, 3)] int option)
+        {
+            var actual = option switch
+                { 
+                    0 => MinValue.Equals(MinValue),
+                    1 => ((Half)12345).Equals(12345),
+                    _ => new Half().Equals(new float())
+                };
+            var expected = option switch
+            {
+                0 => ((float)MinValue).Equals(MinValue),
+                1 => ((float)12345).Equals(12345),
+                _ => new float().Equals(new double())
+            };
+            var test = option switch
+            {
+                0 => $"{MinValue}",
+                1 => $"12345",
+                _ => $"new "
+            };
+            var halfTest = $"{test}{(option != 0 && option != 1 ? "Half()" : "")}";
+            var singleTest = $"{test}{(option != 0 && option != 1 ? "Single()" : "")}";
+            var doubleTest = $"{test}{(option != 0 && option != 1 ? "Double()" : "")}";
+            TestContext.WriteLine($"(Half){halfTest}.Equals({singleTest})\n" +
+                                  $"(Single){singleTest}.Equals({doubleTest});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_finite(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsFinite(target.half);
+            var expected = Single.IsFinite(target.single);
+
+            TestContext.WriteLine($"Half.IsFinite({target.half})\n" +
+                                  $"Single.IsFinite({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_infinity(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsInfinity(target.half);
+            var expected = Single.IsInfinity(target.single);
+
+            TestContext.WriteLine($"Half.IsInfinity({target.half})\n" +
+                                  $"Single.IsInfinity({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_nan(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsNaN(target.half);
+            var expected = Single.IsNaN(target.single);
+
+            TestContext.WriteLine($"Half.IsNaN({target.half})\n" +
+                                  $"Single.IsNaN({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_negative(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsNegative(target.half);
+            var expected = Single.IsNegative(target.single);
+
+            TestContext.WriteLine($"Half.IsNegative({target.half})\n" +
+                                  $"Single.IsNegative({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_negative_infinity(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsNegativeInfinity(target.half);
+            var expected = Single.IsNegativeInfinity(target.single);
+
+            TestContext.WriteLine($"Half.IsNegativeInfinity({target.half})\n" +
+                                  $"Single.IsNegativeInfinity({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_normal(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsNormal(target.half);
+            var expected = Single.IsNormal(target.single);
+
+            TestContext.WriteLine($"Half.IsNormal({target.half})\n" +
+                                  $"Single.IsNormal({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_positive_infinity(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsPositiveInfinity(target.half);
+            var expected = Single.IsPositiveInfinity(target.single);
+
+            TestContext.WriteLine($"Half.IsPositiveInfinity({target.half})\n" +
+                                  $"Single.IsPositiveInfinity({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void is_subnormal(
+                               [ValueSource("ValueTarget")] (Half half, float single) target)
+        {
+            var actual = IsSubnormal(target.half);
+            var expected = Single.IsSubnormal(target.single);
+
+            TestContext.WriteLine($"Half.IsSubnormal({target.half})\n" +
+                                  $"Single.IsSubnormal({target.single});\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void to_string(
+                               [ValueSource("ToStringTargets")] Half target)
+        {
+            var actual = target.ToString();
+            var expected = ((float)target).ToString();
+
+            TestContext.WriteLine($"Half.ToString()\n" +
+                                  $"Single.ToString()\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void to_string(
+                               [ValueSource("ToStringTargets")] Half target,
+                               [ValueSource("FormatTargets")]   string format)
+        {
+            var actual = target.ToString(format);
+            var expected = ((float)target).ToString(format);
+
+            TestContext.WriteLine($"(Half){target}.ToString({format})\n" +
+                                  $"(Single){target}.ToString({format})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void to_string(
+                               [ValueSource("ToStringTargets")] Half target,
+                               [ValueSource("ProviderTargets")] IFormatProvider provider)
+        {
+            var actual = target.ToString(provider);
+            var expected = ((float)target).ToString(provider);
+
+            TestContext.WriteLine($"(Half){target}.ToString({provider})\n" +
+                                  $"(Single){target}.ToString({provider})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void to_string(
+                               [ValueSource("ToStringTargets")] Half target,
+                               [ValueSource("FormatTargets")] string format,
+                               [ValueSource("ProviderTargets")] IFormatProvider provider)
+        {
+            var actual = target.ToString(format, provider);
+            var expected = ((float)target).ToString(format, provider);
+
+            TestContext.WriteLine($"(Half){target}.ToString({format}, {provider})\n" +
+                                  $"(Single){target}.ToString({format}, {provider})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public static void parse(
+                               [ValueSource("ParseTargets")] string target)
+        {
+            var actual = Parse(target);
+            var expected = Single.Parse(target);
+
+            TestContext.WriteLine($"Half.Parse({target})\n" +
+                                  $"Single.Parse({target})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo((Half)expected));
+
+            actual = Parse(target, NumberStyles.Number);
+            expected = Single.Parse(target, NumberStyles.Number);
+
+            TestContext.WriteLine($"Half.Parse({target}, {NumberStyles.Number})\n" +
+                                  $"Single.Parse({target}, {NumberStyles.Number})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo((Half)expected));
+        }
+
+        [Test, Pairwise]
+        public static void parse(
+                               [ValueSource("ParseWithProviderTargets")] (string str, IFormatProvider fp) target)
+        {
+            var actual = Parse(target.str, target.fp);
+            var expected = Single.Parse(target.str, target.fp);
+
+            TestContext.WriteLine($"Half.Parse({target.str}, {target.fp})\n" +
+                                  $"Single.Parse({target.str}, {target.fp})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo((Half)expected));
+            
+            actual = Parse(target.str, NumberStyles.Number, target.fp);
+            expected = Single.Parse(target.str, NumberStyles.Number, target.fp);
+
+            TestContext.WriteLine($"Half.Parse({target.str}, {NumberStyles.Number}, {target.fp})\n" +
+                                  $"Single.Parse({target.str}, {NumberStyles.Number}, {target.fp})\n" +
+                                  $"Half: {actual}\n" +
+                                  $"Single: {expected}");
+
+            Assert.That(actual, Is.EqualTo((Half)expected));
+        }
+    }
+}
